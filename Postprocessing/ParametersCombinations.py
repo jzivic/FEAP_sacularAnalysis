39cd5b0c40309d6a78213ecf2791b66d6a5de59c
jzivic <51597154+jzivic@users.noreply.github.com>
2021-07-15 17:42:04 +0200

    1507-5

--- Postprocessing/ParametersCombinations.py
+++ Postprocessing/ParametersCombinations.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import shutil, os
 from scipy.stats import linregress
 from matplotlib import pyplot as plt
 from Preprocessing.SimulationsData import *
-
-
 
 
 
@@ -18,23 +15,18 @@ L = ABData["L"]
 D = ABData["D"]
 
 d0 = ABData["d0"]
-
 d1 = ABData["d1"]
 d2 = ABData["d2"]
 d3 = ABData["d3"]
 
-d = [d1, d2, d3]
+dSvi = [d0, d1, d2, d3]
 
-# D_d = D-d0
 
 
 range_1 = [-3,-2,-1, 0, 1,2,3]
 range_2 = [1,2]
 range_3 = [1,2,4]
 
-
-allCoeffNames = []
-rVal_all = []
 
 allCoeffs = {"coeffName":[], "r_d0":[], "coeffDict":[]}
 goodCoeffs = {"coeffName":[], "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "coeffDict":[]}
@@ -44,15 +36,7 @@ for iL in range_1:
     for iD in range_1:
         for id in range_1:
 
-            # coeffName_Dict = {"iL": iL, "iD":iD, "id": id}
-            # coeffName_String = "iL="+str(iL)+" iD="+str(iD)+" id="+str(id)
-            # rVal_all.append(rValue)
-            # allCoeffNames.append(coeffName_String)
-            # df_all = pd.DataFrame({ "ime":allCoeffNames, "rValue": rVal_all})
-
-
-
-            parameter = L**iL * D**iD * d0**id
+            parameter = L**iL * D**iD * dSvi[0]**id
             slope, intercept, rValue, pValue, se = linregress(parameter, P)
 
             coeffName = "iL="+str(iL)+" iD="+str(iD)+" id="+str(id)
@@ -61,7 +45,6 @@ for iL in range_1:
             allCoeffs["coeffName"].append(coeffName)
             allCoeffs["r_d0"].append(rValue)
             allCoeffs["coeffDict"].append(coeffName_Dict)
-
 
             df_all = pd.DataFrame(allCoeffs)
 
@@ -74,24 +57,33 @@ for iL in range_1:
                 goodCoeffs["coeffDict"].append(coeffName_Dict)
 
 
-                par_d1 = L**iL * D**iD * d1**id
-                slope, intercept, rValue1, pValue, se = linregress(par_d1, P)
-                goodCoeffs["r_d1"].append(rValue1)
 
-                par_d2 = L**iL * D**iD * d2**id
-                slope, intercept, rValue2, pValue, se = linregress(par_d2, P)
-                goodCoeffs["r_d2"].append(rValue)
-
-                par_d3 = L**iL * D**iD * d3**id
-                slope, intercept, rValue3, pValue, se = linregress(par_d3, P)
-                goodCoeffs["r_d3"].append(rValue3)
+                rSvi = []
+                for i in range(1,len(dSvi)):
+                    parameterGood = L ** iL * D ** iD * dSvi[i] ** id
+                    slope, intercept, rValue, pValue, se = linregress(parameterGood, P)
+                    rSvi.append(rValue)
 
 
-            df_high = pd.DataFrame(goodCoeffs)
+                goodCoeffs["r_d1"].append(rSvi[1-1])
+                goodCoeffs["r_d2"].append(rSvi[2-1])
+                goodCoeffs["r_d3"].append(rSvi[3-1])
+
+            df_good = pd.DataFrame(goodCoeffs)
 
 
 
-# print(df_high)
+
+
+
+
+
+
+
+
+
+
+print(df_good)
 # print(df_all)
 
 
--- Postprocessing/ParametersCombinationsCopy.py
+++ Postprocessing/ParametersCombinationsCopy.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import shutil, os
 from scipy.stats import linregress
 from matplotlib import pyplot as plt
 from Preprocessing.SimulationsData import *
-
-
 
 
 
@@ -18,14 +15,13 @@ L = ABData["L"]
 D = ABData["D"]
 
 d0 = ABData["d0"]
-
 d1 = ABData["d1"]
 d2 = ABData["d2"]
 d3 = ABData["d3"]
 
-d = [d1, d2, d3]
+Dmd = D - d0
 
-# D_d = D-d0
+dSvi = [d0, d1,d2,d3]
 
 
 range_1 = [-3,-2,-1, 0, 1,2,3]
@@ -33,60 +29,73 @@ range_2 = [1,2]
 range_3 = [1,2,4]
 
 
-allCoeffNames = []
-rVal_all = []
-
-goodCoeffname = []
-goodCoeff_Dict = []
-rVal_good = { "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "coeffs":[]}
-
-
-
+allCoeffs = {"coeffName":[], "r_d0":[], "coeffDict":[]}
+goodCoeffs = {"coeffName":[], "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "coeffDict":[]}
 
 
 for iL in range_1:
     for iD in range_1:
         for id in range_1:
 
-            coeffName_Dict = {"iL": iL, "iD":iD, "id": id}
-            coeffName_String = "iL="+str(iL)+" iD="+str(iD)+" id="+str(id)
-
-
             parameter = L**iL * D**iD * d0**id
             slope, intercept, rValue, pValue, se = linregress(parameter, P)
 
-            rVal_all.append(rValue)
-            allCoeffNames.append(coeffName_String)
+            coeffName = "iL="+str(iL)+" iD="+str(iD)+" id="+str(id)
+            coeffName_Dict = {"iL": iL, "iD":iD, "id": id}
 
-            df_all = pd.DataFrame({ "ime":allCoeffNames, "rValue": rVal_all})
+            allCoeffs["coeffName"].append(coeffName)
+            allCoeffs["r_d0"].append(rValue)
+            allCoeffs["coeffDict"].append(coeffName_Dict)
+
+            df_all = pd.DataFrame(allCoeffs)
 
 
 
             if abs(rValue) > 0.85:
-                goodCoeffname.append(coeffName_String)
 
-                rVal_good["coeffs"].append(coeffName_Dict)
+                goodCoeffs["coeffName"].append(coeffName)
+                goodCoeffs["r_d0"].append(rValue)
+                goodCoeffs["coeffDict"].append(coeffName_Dict)
 
-                rVal_good["r_d0"].append(rValue)
+
+
+                # def parametar_d(dVel):
+                #     par_d1 = L ** iL * D ** iD * dVel ** id
+                #     return par_d1
+                # parametar_d(d1)
 
                 par_d1 = L**iL * D**iD * d1**id
-                slope, intercept, rValue, pValue, se = linregress(par_d1, P)
-                rVal_good["r_d1"].append(rValue)
+                slope, intercept, rValue1, pValue, se = linregress(par_d1, P)
+                goodCoeffs["r_d1"].append(rValue1)
 
                 par_d2 = L**iL * D**iD * d2**id
-                slope, intercept, rValue, pValue, se = linregress(par_d2, P)
-                rVal_good["r_d2"].append(rValue)
+                slope, intercept, rValue2, pValue, se = linregress(par_d2, P)
+                goodCoeffs["r_d2"].append(rValue)
 
                 par_d3 = L**iL * D**iD * d3**id
-                slope, intercept, rValue, pValue, se = linregress(par_d3, P)
-                rVal_good["r_d3"].append(rValue)
-
-
-            df_high = pd.DataFrame( rVal_good, index=goodCoeffname)
+                slope, intercept, rValue3, pValue, se = linregress(par_d3, P)
+                goodCoeffs["r_d3"].append(rValue3)
 
 
 
-print(df_high)
+                for dIde in dSvi:
+                    par_d1 = L ** iL * D ** iD * d1 ** id
+                    slope, intercept, rValue1, pValue, se = linregress(par_d1, P)
+                    goodCoeffs["r_d1"].append(rValue1)
+
+
+
+
+
+
+
+
+
+            df_good = pd.DataFrame(goodCoeffs)
+
+
+
+# print(df_good)
 # print(df_all)
 
 
