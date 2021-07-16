60339bab8e9687210cb7e3ad2ca0f14a386a68b3
jzivic <51597154+jzivic@users.noreply.github.com>
2021-07-16 11:28:29 +0200

    1607-3

--- Postprocessing/ParametersCombinations.py
+++ Postprocessing/ParametersCombinations.py
@@ -5,101 +5,6 @@ from Preprocessing.SimulationsData import *
 
 
 
-ABData = pd.read_pickle(PickleData_AB)
-
-
-
-
-
-
-
-range_1 = [-3,-2,-1, 0, 1,2,3]
-range_12 = [-3,-2,-1, 1,2,3]
-
-# range_2 = [1,2]
-range_2 = [1,2,4]
-
-
-allCoeffs = {"coeffName":[], "r_d0":[], "coeffDict":[]}
-goodCoeffs = {"coeffName":[], "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "coeffDict":[]}
-
-
-
-def Calculate_rValue(inputPickle):
-    P = inputPickle["P"]
-    L = inputPickle["L"]
-    D = inputPickle["D"]
-    d0 = inputPickle["d0"]
-    d1 = inputPickle["d1"]
-    d2 = inputPickle["d2"]
-    d3 = inputPickle["d3"]
-
-    dSvi = [d0, d1, d2, d3]
-    
-    
-    
-    for iL in range_1:
-        for jD in range_1:
-            for kd in range_1:
-                # for lDd in range_12:
-                for mD_d in range_1:
-                    for N in range_2:
-    
-    
-                        parameter = L**iL * D**jD * dSvi[0]**kd * (N*D**mD_d-dSvi[0]**mD_d)
-    
-                        slope, intercept, rValue, pValue, se = linregress(parameter, P)
-    
-                        coeffName = "iL="+str(iL)+" jD="+str(jD)+" kd="+str(kd)+" mD_d="+str(mD_d)+" N="+str(N)
-                        coeffName_Dict = {"iL": iL, "jD":jD, "kd": kd, "mD_d":mD_d, "N":N}
-    
-            
-                        allCoeffs["coeffName"].append(coeffName)
-                        allCoeffs["r_d0"].append(rValue)
-                        allCoeffs["coeffDict"].append(coeffName_Dict)
-    
-                        df_all = pd.DataFrame(allCoeffs)
-    
-    
-    
-                        if abs(rValue) > 0.85:
-    
-                            goodCoeffs["coeffName"].append(coeffName)
-                            goodCoeffs["r_d0"].append(rValue)
-                            goodCoeffs["coeffDict"].append(coeffName_Dict)
-    
-                            rSvi = []
-                            for i in range(1,len(dSvi)):
-                                parameterGood = L ** iL * D ** jD * dSvi[0] ** kd * (N * D ** mD_d - dSvi[0] ** mD_d)
-                                slope, intercept, rValue, pValue, se = linregress(parameterGood, P)
-                                rSvi.append(rValue)
-    
-    
-                            goodCoeffs["r_d1"].append(rSvi[1-1])
-                            goodCoeffs["r_d2"].append(rSvi[2-1])
-                            goodCoeffs["r_d3"].append(rSvi[3-1])
-
-
-
-    df_good = pd.DataFrame(goodCoeffs)
-
-    goodCoeffs["rSuma"] = (goodCoeffs["r_d0"] + goodCoeffs["r_d1"] + goodCoeffs["r_d2"] + goodCoeffs["r_d3"])
-
-    df_all.to_pickle(ParametersCombinations_all)
-    df_good.to_pickle(ParametersCombinations_good)
-
-
-
-# Calculate_rValue(ABData)
-
-
-
-
-
-
-
-
-
 
 
 
@@ -119,7 +24,7 @@ class ParameterCombinations:
         self.range_2 = [1, 2, 4]
 
         self.allCoeffs = {"coeffName":[], "r_d0":[], "coeffDict":[]}
-        self.goodCoeffs = {"coeffName":[], "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "coeffDict":[]}
+        self.goodCoeffs = {"coeffName":[], "r_d0":[], "r_d1":[], "r_d2":[], "r_d3":[], "rSuma":[], "coeffDict":[]}
 
 
         self.P = self.inputData["P"]
@@ -137,12 +42,12 @@ class ParameterCombinations:
 
     def Calculate_rValue(self):
 
-        for iL in range_1:
-            for jD in range_1:
-                for kd in range_1:
+        for iL in self.range_1:
+            for jD in self.range_1:
+                for kd in self.range_1:
                     # for lDd in range_12:
-                    for mD_d in range_1:
-                        for N in range_2:
+                    for mD_d in self.range_1:
+                        for N in self.range_2:
 
                             parameter = self.L ** iL * self.D ** jD * self.dSvi[0] ** kd * (N * self.D ** mD_d - self.dSvi[0] ** mD_d)
                             slope, intercept, rValue, pValue, se = linregress(parameter, self.P)
@@ -151,34 +56,50 @@ class ParameterCombinations:
                                         mD_d) + " N=" + str(N)
                             coeffName_Dict = {"iL": iL, "jD": jD, "kd": kd, "mD_d": mD_d, "N": N}
 
-                            allCoeffs["coeffName"].append(coeffName)
-                            allCoeffs["r_d0"].append(rValue)
-                            allCoeffs["coeffDict"].append(coeffName_Dict)
+                            self.allCoeffs["coeffName"].append(coeffName)
+                            self.allCoeffs["r_d0"].append(rValue)
+                            self.allCoeffs["coeffDict"].append(coeffName_Dict)
 
                             if abs(rValue) > 0.85:
-                                goodCoeffs["coeffName"].append(coeffName)
-                                goodCoeffs["r_d0"].append(rValue)
-                                goodCoeffs["coeffDict"].append(coeffName_Dict)
+                                self.goodCoeffs["coeffName"].append(coeffName)
+                                self.goodCoeffs["r_d0"].append(rValue)
+                                self.goodCoeffs["coeffDict"].append(coeffName_Dict)
 
                                 rSvi = []
                                 for i in range(1, len(self.dSvi)):
                                     parameterGood = self.L ** iL * self.D ** jD * self.dSvi[i] ** kd * (
                                                 N * self.D ** mD_d - self.dSvi[i] ** mD_d)
 
-                                    slope, intercept, rValue, pValue, se = linregress(parameterGood, self.P)
-                                    rSvi.append(rValue)
+                                    slope, intercept, rValueRest, p, se = linregress(parameterGood, self.P)
+                                    rSvi.append(rValueRest)
 
-                                goodCoeffs["r_d1"].append(rSvi[1 - 1])
-                                goodCoeffs["r_d2"].append(rSvi[2 - 1])
-                                goodCoeffs["r_d3"].append(rSvi[3 - 1])
+                                self.goodCoeffs["r_d1"].append(rSvi[1 - 1])
+                                self.goodCoeffs["r_d2"].append(rSvi[2 - 1])
+                                self.goodCoeffs["r_d3"].append(rSvi[3 - 1])
+
+                                rS = (sum([abs(i) for i in rSvi]) + abs(rValue))/4
+                                self.goodCoeffs["rSuma"].append(rS)
 
 
-        df_all = pd.DataFrame(allCoeffs)
+
+
+
+
+        df_all = pd.DataFrame(self.allCoeffs)
+
+
+
+        df_good = pd.DataFrame(self.goodCoeffs)
+
+
+        # print(len(self.goodCoeffs["r_d0"]))
+        # print(len(self.goodCoeffs["r_d1"]))
+        # print(len(self.goodCoeffs["r_d2"]))
+        # print(len(self.goodCoeffs["r_d3"]))
+        # print(len(self.goodCoeffs["rSuma"]))
+
+
         df_all.to_pickle(ParametersCombinations_all)
-
-        goodCoeffs["rSuma"] = goodCoeffs["r_d0"] + goodCoeffs["r_d1"] + goodCoeffs["r_d2"] + goodCoeffs["r_d3"]
-        df_good = pd.DataFrame(goodCoeffs)
-
         df_good.to_pickle(ParametersCombinations_good)
 
 
